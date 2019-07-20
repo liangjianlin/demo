89d274530353afecc7c065a9a501aad3461b54cb
liangjianlin <nbaliangjianlin@qq.com>
2019-07-20 17:41:22 +0800

    init

--- build.sh
+++ build.sh
@@ -2,3 +2,4 @@ cd /root/svn/demo
 git pull https://github.com/liangjianlin/demo.git
 mvn package
 docker build --force-rm -t springboot-app .
+docker run -d -p 8080:8080 -u root -e TZ="Aisa/Shanghai" --name springboot-app springboot-app
