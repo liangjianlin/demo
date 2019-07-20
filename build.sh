e1e6e9e415d5a385551e85fb155fec019fe3810b
liangjianlin <nbaliangjianlin@qq.com>
2019-07-20 17:49:33 +0800

    init

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ mvn package
 docker image rm -f springboot-app
 docker build --force-rm -t springboot-app .
 docker container rm -f springboot-app
-docker run -d -p 8888:8080 -u root -e TZ="Aisa/Shanghai" --name springboot-app springboot-app
+docker run -d -p 3333:8080 -u root -e TZ="Aisa/Shanghai" --name springboot-app springboot-app
