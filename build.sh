c9cb4f41edc75b24e585ef19c2acb94485931ed8
liangjianlin <nbaliangjianlin@qq.com>
2019-07-21 11:49:43 +0800

    init

--- build.sh
+++ build.sh
@@ -1,4 +1,2 @@
-cd /root/svn/demo
-git pull https://github.com/liangjianlin/demo.git
 mvn package
 sh build-image.sh
---
+++ update.sh
@@ -0,0 +1,3 @@
+cd /root/svn/demo
+git pull https://github.com/liangjianlin/demo.git
+sh build.sh
