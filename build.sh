6c440b304241e7bd0a3e7205097c1ca5e9d46e11
liangjianlin <nbaliangjianlin@qq.com>
2019-07-21 11:42:54 +0800

    init

--- build.sh
+++ build.sh
@@ -1,7 +1,4 @@
 cd /root/svn/demo
 git pull https://github.com/liangjianlin/demo.git
 mvn package
-docker container exec springboot-app ps -ef | grep demo-0.0.1-SNAPSHOT.jar | cut -c 5 | xargs kill -s 9
-docker container exec springboot-app rm -f demo-0.0.1-SNAPSHOT.jar
-docker container exec springboot-app mv ./target/demo-0.0.1-SNAPSHOT.jar .
-docker container exec springboot-app java -jar demo-0.0.1-SNAPSHOT.jar
+sh build-image.sh
